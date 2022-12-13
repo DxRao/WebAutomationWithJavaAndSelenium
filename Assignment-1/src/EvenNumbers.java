abd559285d959bb3bb3d8456a4e6ffff95935a4a
DxRao <tdayakarrao@gmail.com>
2022-12-13 09:39:58 +0530

    Assignment-1: Task-4

---
+++ Assignment-1/src/EvenNumbers.java
@@ -0,0 +1,18 @@
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i < 201; i++) {
+			
+			if(i % 2 == 0) {
+				
+				System.out.print(i + " ");
+				
+			}
+			
+		}
+
+	}
+
+}
