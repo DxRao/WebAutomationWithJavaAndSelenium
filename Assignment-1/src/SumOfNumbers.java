70e589c0672141c94bcb9604973668c25de46704
DxRao <tdayakarrao@gmail.com>
2022-12-13 08:43:29 +0530

    Assignment-1: Task-2

---
+++ Assignment-1/src/SumOfNumbers.java
@@ -0,0 +1,12 @@
+
+public class SumOfNumbers {
+
+	public static void main(String[] args) {
+		
+		double total = 0.0;
+		total = 10 + 90.78 + 111 + 8989 + 7876;
+		System.out.println("Sum of the given 5 numbers is: " + total);
+		
+	}
+
+}
