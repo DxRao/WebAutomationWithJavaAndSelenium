a858f7932267d19b0477348e87e272892993b9e1
DxRao <tdayakarrao@gmail.com>
2022-12-13 09:25:38 +0530

    Assignment-1: Task-3

---
+++ Assignment-1/src/AverageOfNumbers.java
@@ -0,0 +1,18 @@
+
+public class AverageOfNumbers {
+
+	public static void main(String[] args) {
+		
+		double total = 0.00;
+		double average = 0.00;
+		double roundOff = 0.00;
+		
+		total = 10 + 90.78 + 111 + 8989 + 7876;
+		
+		average = total / 5.0;
+		System.out.println("Average of given 5 numbers is: " + average);
+		
+		
+	}
+
+}
