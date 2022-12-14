130aa24bf6db772123a9359acdaf50a2084a9df3
DxRao <tdayakarrao@gmail.com>
2022-12-14 23:31:58 +0530

    Assignment-1: Task-8

---
+++ Assignment-1/src/StudentMarks.java
@@ -0,0 +1,17 @@
+
+public class StudentMarks {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {78, 12, 89, 55, 35};
+		
+		for(int ary: arr) {
+			
+			if(ary > 80) {
+				
+				System.out.println("Marks scored above 80 by students are: "+ ary);				
+			}			
+		}
+	}
+
+}
