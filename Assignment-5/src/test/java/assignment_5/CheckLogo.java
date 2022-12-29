06279feca9376660bfcc06d7471113221a615cb8
D Rao <tdayakarrao@gmail.com>
2022-12-29 08:09:24 +0530

    Assignment-5: Task-2

---
+++ Assignment-5/src/test/java/assignment_5/CheckLogo.java
@@ -0,0 +1,33 @@
+// Assignment-5: Task-2
+package assignment_5;
+
+import java.util.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CheckLogo {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://www.amazon.in");
+		
+		//driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");		
+		
+		
+		List<WebElement> links = driver.findElements(By.tagName("img"));	    
+		 
+	        for(WebElement ele:links){
+	        	
+	            System.out.println(ele.getAttribute("src"));
+	        }
+	    		
+		
+		// driver.close();	
+	}
+	
+}
+
